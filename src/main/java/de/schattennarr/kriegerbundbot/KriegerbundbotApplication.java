1363c1cf8ecfcfdf27faad7fae7a2c66844c3638
Maik <heunisch@posteo.de>
2021-06-22 11:00:18 +0200

    intial commit

--- src/main/java/de/schattennarr/kriegerbundbot/KriegerbundbotApplication.java
+++ src/main/java/de/schattennarr/kriegerbundbot/KriegerbundbotApplication.java
@@ -1,10 +1,14 @@
 package de.schattennarr.kriegerbundbot;
 
+import net.dv8tion.jda.api.JDABuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KriegerbundbotApplication {
+
+	String token;
+	JDABuilder builder = JDABuilder.createDefault(token);
 
 	public static void main(String[] args) {
 		SpringApplication.run(KriegerbundbotApplication.class, args);
