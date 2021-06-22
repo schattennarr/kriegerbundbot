4b49cd61dd736524ef7dc21a889efbe3c5abe2d1
Maik <heunisch@posteo.de>
2021-06-22 10:20:43 +0200

    intial commit

---
+++ src/main/java/de/schattennarr/kriegerbundbot/KriegerbundbotApplication.java
@@ -0,0 +1,13 @@
+package de.schattennarr.kriegerbundbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KriegerbundbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KriegerbundbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/schattennarr/kriegerbundbot/KriegerbundbotApplicationTests.java
@@ -0,0 +1,13 @@
+package de.schattennarr.kriegerbundbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KriegerbundbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
